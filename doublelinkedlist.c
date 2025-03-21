fbfdc520f52593eea5dd1abc7f443e69ff5ebd01
faizaan7087 <faizaan375@gmail.com>
2025-03-21 15:32:25 +0530

    double linked list
    its a double linked list

---
+++ doublelinkedlist.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+
+    int data;
+    struct node *next;
+    struct node *prev;
+
+};
+typedef struct node dnode;
+dnode *head = NULL;
+void createlist(int x)
+{
+
+    dnode *newnode = NULL;
+    newnode = (dnode*)malloc(sizeof(dnode));
+    newnode -> data = x;
+    newnode -> next = NULL;
+    newnode -> prev = NULL;
+    head = newnode;
+
+}
+void appendnode(int x)
+{
+
+    dnode *tempnode = head;
+    dnode *newnode = NULL;
+    newnode=(dnode*)malloc(sizeof(dnode));
+    newnode -> data = x;
+    newnode -> next = NULL;
+    if(tempnode == NULL)
+    {
+
+        head = newnode;
+        return;
+    }
+    while(tempnode->next != NULL)
+    {
+
+        tempnode = tempnode->next;
+
+    }
+    tempnode ->next = newnode;
+    newnode -> prev = tempnode;
+}
+void printlist(void)
+{
+
+    dnode *tempnode = head;
+    if(tempnode == NULL)
+    {
+
+        printf("the list is empty\n");
+    }
+    while(tempnode != NULL)
+    {
+
+        printf("%d\n", tempnode ->data);
+        tempnode = tempnode -> next;
+    }
+
+}
+void insertab(int x)
+{
+
+    dnode *tempnode = head;
+    dnode *newnode = NULL;
+    newnode =(dnode*)malloc(sizeof(dnode));
+    newnode -> data = x;
+    tempnode -> prev = newnode;
+    newnode -> next = tempnode;
+    newnode -> prev = NULL;
+    head = newnode;
+}
+void delfnode(void)
+{
+
+    dnode *tempnode = head;
+    head = tempnode-> next;
+    free(tempnode);
+    printf("the first element is deleted\n");
+}
+void dellnode(void)
+{
+
+    dnode *tempnode = head;
+    dnode *collect = NULL;
+    while(tempnode -> next->next != NULL)
+    {
+
+        tempnode = tempnode->next;
+
+    }
+    tempnode -> next =collect;
+    free(collect);
+    printf("the last node deleted\n");
+
+}
+void search(int x)
+{
+
+    dnode *tempnode = head;
+    while ( tempnode -> data != x)
+    {
+
+        tempnode = tempnode->next;
+        return printf("the element not found\n");
+    }
+    printf("the element found\n");
+
+}
+void main()
+{
+
+    createlist(10);
+    appendnode(20);
+    insertab(30);
+    printlist();
+    delfnode();
+    dellnode();
+    printlist();
+    search(10);
+
+}
