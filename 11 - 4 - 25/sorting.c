77e870c7cbf0018747b4621f84711901b054e69e
faizaan7087 <faizaan375@gmail.com>
2025-04-11 15:43:48 +0530

    Add files via upload

--- 11 - 4 - 25/sorting.c
+++ 11 - 4 - 25/sorting.c
@@ -17,7 +17,65 @@ void insertionsort(int a[],int n)
         a[prev + 1] = cur;
 
     }
+    printf("Insertion sort \n");
     for(int i = 0 ; i<n ; i++)
+        {
+
+            printf("%d ",a[i]);
+        }
+}
+
+void bubblesort(int a[], int n)
+{
+
+    int temp = 0;
+    for(int i = 0 ; i<n ; i++)
+    {
+        for(int j = 0 ; j<i-n-1; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = temp;
+
+            }
+
+        }
+
+    }
+    printf("\nBubble sort \n");
+   for(int i = 0 ; i<n ; i++)
+        {
+
+            printf("%d ",a[i]);
+        }
+}
+void selectionsort(int a[], int n)
+{
+    int temp=0;
+    for(int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i+1 ; j<n ; j++)
+        {
+            if(a[j] < a[min])
+            {
+
+            min = j;
+            }
+
+        }
+        if( i!= a[min])
+        {
+            temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+
+        }
+    }
+    printf("\nSelection sort \n");
+   for(int i = 0 ; i<n ; i++)
         {
 
             printf("%d ",a[i]);
@@ -29,5 +87,7 @@ void main()
     int a[]={5 , 4, 3 , 2, 1};
     int n = sizeof(a)/sizeof(int);
     insertionsort(a , n);
+    bubblesort( a , n);
+    selectionsort( a , n);
 
 }
